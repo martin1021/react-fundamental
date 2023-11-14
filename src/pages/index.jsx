79e453d41b1de058a30403c008590e98dd064706
Martinus Rombe <55009563+martin1021@users.noreply.github.com>
2023-11-14 15:57:58 +0800

    Update Total Post

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,18 +5,20 @@ import Article from "../components/Article.jsx";
 
 function HomePage() {
     const [posts, setPosts] = useState(postsData);
+    const [totalPosts, setTotalPosts] = useState(0);
 
     const onSearchChange = (value) => {
         const filteredPosts = postsData.filter((item) => 
             item.title.includes(value)
         )
         setPosts(filteredPosts);
+        setTotalPosts(filteredPosts.length);
     }
 
     return (
         <>
         <h1>Blog</h1>
-        <Search onSearchChange={onSearchChange} />
+        <Search onSearchChange={onSearchChange} totalPosts={totalPosts}/>
             {posts.map((props,index) => (
                     <>
                     <Article title={props.title} tags={props.tags} date={props.date} key={index}/>
